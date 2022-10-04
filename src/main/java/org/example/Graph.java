bae95339037d6fb5ec252af13b729600ee61074a
bai_777 <bai_777@mail.ru>
2022-10-04 18:20:57 +0300

    начал 7е дз

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public interface Graph {
-    
+    void addVertex(String label);
+    boolean addEdge(String startLabel, String secondLabel, int distance);
+    int getSize();
+    int findShortWay(String start, String end);
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+
 }
